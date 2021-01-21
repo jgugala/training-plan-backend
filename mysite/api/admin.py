d306afbe993273263dc745eb6a2a42c2aae33d18
Jakub Gugała <jg.hpk.solutions@gmail.com>
2021-01-21 17:36:06 +0100

    Base models defined.

--- mysite/api/admin.py
+++ mysite/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Exercise, Training, TrainingItem
+
+admin.site.register(Exercise)
+admin.site.register(Training)
+admin.site.register(TrainingItem)
+
--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+from positions import PositionField
+
+
+class BaseModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    ``created_at`` and ``updated_at`` fields.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Exercise(BaseModel):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    file = models.FileField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Training(BaseModel):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class TrainingItem(BaseModel):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    training = models.ForeignKey(Training, on_delete=models.CASCADE)
+    position = PositionField(collection='training')
+    reps_count = models.IntegerField()
+
+    class Meta:
+        ordering = ['training', 'position']
